9ca78421c34b145c1ba38941e02882bf3536f86e
Alin Andrei <alin.andrei1016@gmail.com>
2025-12-01 13:16:15 +0100

    input styl

---
+++ resources/build/assets/addon-CCN_4Xvx.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./RegistrationNumberValidator-kSJKVbTP.js";Statamic.booting(()=>{Statamic.$components.register("registration-number-validator",t)});
--- resources/js/components/fieldtypes/RegistrationNumberValidator.vue
+++ resources/js/components/fieldtypes/RegistrationNumberValidator.vue
@@ -182,7 +182,7 @@ async function validateNumber() {
                     v-model="number"
                     @update:modelValue="number = $event"
                     :placeholder="props.placeholder"
-                    class="w-full px-3 py-2 text-sm text-gray-700 border border-gray-700 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500"
+                    class="tracking-wide shadow-xs bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 />
             </div>
 
